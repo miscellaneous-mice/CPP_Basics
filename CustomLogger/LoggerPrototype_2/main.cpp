4a29bc17b3b3715a548cac39fd704383a58705cd
prateek <prateek@frog.local>
2024-11-03 00:14:02 +0530

    Made the logger with more defined examples

--- CustomLogger/LoggerPrototype_2/main.cpp
+++ CustomLogger/LoggerPrototype_2/main.cpp
@@ -2,11 +2,11 @@
 
 int main(){
     Logger* logger = new Logger("Test");
-    LoggerHelper::setLevel(logger, Configs::LogLevelWarning);
+    LoggerHelper::setLevel(logger, Configs::LogLevelInfo);
     LoggerHelper::setFormatter(logger, "date||level||message");
     logger->info("Hello world");
-    BaseUtils* consoleutils = logger->StreamHandler(Configs::LogLevelWarning, "%d-%m-%Y %H-%M-%S", "date::level::message");
-    BaseUtils* fileutils = logger->FileHandler(Configs::LogLevelError, "%d:%m:%Y %H:%M:%S", "date::level::message");
+    BaseUtils* consoleutils = logger->StreamHandler(Configs::LogLevelError, "%d-%m-%Y %H-%M-%S", "date::level::message");
+    BaseUtils* fileutils = logger->FileHandler(Configs::LogLevelWarning, "%d:%m:%Y %H:%M:%S", "date::level::message");
     logger->addHandler(consoleutils);
     LoggerHelper::setFormatter(fileutils, "date--level--message");
     logger->addHandler(fileutils);
